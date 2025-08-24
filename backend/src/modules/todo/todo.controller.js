48b5de3172ca27fd3fa3bdb7100490b9a0511687
ebbryan <earlbryanburaay@gmail.com>
2025-08-24 20:53:07 +0800

    added createTodo in controller, next is to write the route

--- backend/src/modules/todo/todo.controller.js
+++ backend/src/modules/todo/todo.controller.js
@@ -1,3 +1,4 @@
+const { todoId } = require("./todo-in-memory-model.js");
 const todoService = require("./todo.service.js");
 
 async function getAllTodos(req, res) {
@@ -9,4 +10,15 @@ async function getAllTodos(req, res) {
   }
 }
 
-module.exports = { getAllTodos };
+async function createTodo(req, res) {
+  try {
+    const initialPayload = req.body;
+    const payload = { id: todoId++, ...initialPayload };
+    const response = await todoService.createTodo(payload);
+    return res.json({ data: response, success: true });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
+}
+
+module.exports = { getAllTodos, createTodo };
