624ff2399791d4ba501e28ed4a9dc2b8b5ab70b4
ebbryan <earlbryanburaay@gmail.com>
2025-08-24 20:24:13 +0800

    added global not found error handling

--- backend/in-memory-model/todo.js
+++ backend/in-memory-model/todo.js
@@ -1,2 +1,4 @@
+const { v4: uuidv4 } = require("uuid");
+
 export let todos = [];
-export let nextId = 1;
+export let todoId = uuidv4();
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
+app.use((req, res) => {
+  res.status(404).send({
+    error: { message: "Endpoint not found!", status: res.statusCode },
+  });
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Backend is running on http://localhost:${PORT}`);
